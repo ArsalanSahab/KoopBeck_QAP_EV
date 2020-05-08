96fdf816dbcd0e2cd9810b3a134b3bf19340a3b9
Muhammad Arsalan <arsalan@Muhammads-MacBook-Pro-2.local>
2020-05-09 02:58:19 +0500

    Added some final tweaks

--- configurations.py
+++ configurations.py
@@ -1,6 +1,6 @@
 ########## BASIC CONFIGURATIONS SETTINGS ############
 
-DATA_SET = "had12.dat" # Data Sets Taken From : http://anjos.mgi.polymtl.ca/qaplib/inst.html#HRW
+DATA_SET = "dataset_1.dat" # Data Sets Taken From : http://anjos.mgi.polymtl.ca/qaplib/inst.html#HRW
 CROSSOVER_PERCENTAGE = 0.7
 POPULATION_SIZE = 70
 NUMBER_OF_GENERATIONS = 20
